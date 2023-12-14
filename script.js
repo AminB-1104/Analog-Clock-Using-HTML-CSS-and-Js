7cba17859d76fdebae70d78b1b69fc8911f41e5b
AminB-1104 <148558228+AminB-1104@users.noreply.github.com>
2023-12-15 01:05:15 +0500

    Fixed Alignments
    Aligned hour, minute and second hands to the center point of clock as best as I could

---
+++ script.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+  time = new Date();
+
+  htime = time.getHours();
+  mtime = time.getMinutes();
+  stime = time.getSeconds();
+
+  hRotation = 30 * htime + mtime / 2;
+  mRotation = 6 * mtime;
+  sRotation = 6 * stime;
+
+  hour.style.transform = `rotate(${hRotation}deg)`;
+  minute.style.transform = `rotate(${mRotation}deg)`;
+  second.style.transform = `rotate(${sRotation}deg)`;
+}, 0);
